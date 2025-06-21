e51cdfe020fea44e8bc63a27e40bc3860b49ea9b
Ricardo <ricardohsmello@gmail.com>
2025-06-20 22:26:24 -0300

    adjusting Function

--- src/main/resources/META-INF/resources/assets/js/function_find_similar.js
+++ src/main/resources/META-INF/resources/assets/js/function_find_similar.js
@@ -1,18 +1,1 @@
-async function fetchAndDisplayMovies(data) {
-    try {
-        const resultsDiv = document.getElementById('movieResults');
-        resultsDiv.innerHTML = '<div class="loading"><span class="badge bg-light text-dark" style="font-size: 30px">Loading movies...</span></div>';
-
-        const response = await fetch('https://movies-finder-vsnw.onrender.com/movies/findSimilar', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
-        return await response.json();
-    } catch (error) {
-        console.error('Error:', error);
-        return [];
-    }
-}
+// TODO
